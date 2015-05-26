dd1ab4aea11df2de32e1dad25ee0d6e323318db8
flbe <flbe@zuehlke.com>
2015-05-26 16:32:12 +0200

    Step 4: Simplified currentCategory, simplified wasCorrectlyAnswered

--- src/main/java/com/adaptionsoft/games/uglytrivia/Game.java
+++ src/main/java/com/adaptionsoft/games/uglytrivia/Game.java
@@ -108,30 +108,18 @@ public class Game {
 	
 	
 	private String currentCategory() {
-		//duplicate lines / strings
-		if (places[currentPlayer] == 0) return "Pop";
-		if (places[currentPlayer] == 4) return "Pop";
-		if (places[currentPlayer] == 8) return "Pop";
-		if (places[currentPlayer] == 1) return "Science";
-		if (places[currentPlayer] == 5) return "Science";
-		if (places[currentPlayer] == 9) return "Science";
-		if (places[currentPlayer] == 2) return "Sports";
-		if (places[currentPlayer] == 6) return "Sports";
-		if (places[currentPlayer] == 10) return "Sports";
+		if (places[currentPlayer] % 4 == 0) return "Pop";
+		if (places[currentPlayer] % 4 == 1) return "Science";
+		if (places[currentPlayer] % 4 == 2) return "Sports";
 		return "Rock";
 	}
 
 	public boolean wasCorrectlyAnswered() {
-		if (inPenaltyBox[currentPlayer]){
-			if (isGettingOutOfPenaltyBox) {
-				return addPursesAndDidPlayerWin();
-			} else {
-				nextPlayer();
-				return true;
-			}
-		} else {
-			return addPursesAndDidPlayerWin();
+		if (inPenaltyBox[currentPlayer] && !isGettingOutOfPenaltyBox){
+			nextPlayer();
+			return true;
 		}
+		return addPursesAndDidPlayerWin();
 	}
 
 	private boolean addPursesAndDidPlayerWin() {
